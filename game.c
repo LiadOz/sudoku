5c6f3013d9326cdb1437eeb08622467bd1ff413a
liadozil@gmail.com <LiadOzIL@Gmail.Com>
2019-12-26 10:35:05 +0200

    added the set and hint ocmmands added shallow validation for coordinate

--- game.c
+++ game.c
@@ -1,7 +1,4 @@
 #include <stdlib.h>
-/*
- * temp include for debugging
- */
 #include <stdio.h>
 
 /*
@@ -30,9 +27,9 @@ void init_board(Board* b, int width, int height){
     b->state = malloc(width * height * sizeof(int *));
     b->fixed = malloc(width * height * sizeof(int *));
     for(i = 0; i < width * height; i++){
-        b->solution[i] = malloc(width * height * sizeof(int));
-        b->state[i] = malloc(width * height * sizeof(int)); 
-        b->fixed[i] = malloc(width * height * sizeof(int));
+        b->solution[i] = calloc(width * height, sizeof(int));
+        b->state[i] = calloc(width * height, sizeof(int)); 
+        b->fixed[i] = calloc(width * height, sizeof(int));
     }
     b->width = width;
     b->height = height;
@@ -53,20 +50,36 @@ void free_board(Board* b){
     free(b->fixed);
 }
 
+/* checks if number exists in array if true return 1 */
+int number_in_array(int* arr, int number, int size){
+    int i;
+    for(i = 0; i < size; i++){
+        if(arr[i] == number){
+            return 1;
+        }
+    }
+    return 0;
+}
+
 /* Returns array of all the numbers in the row
  * assumes row is in the range
+ * dynamically allocate memory must be freed
  */
 int* get_row_numbers(Board* b, int row){
-    /* not good if user frees then the board is invalid */
-    return b->state[row];
+    int *arr = malloc(b->size * sizeof(int));
+    int i;
+    for(i = 0; i < b->size; i++){
+        arr[i] = b->state[row][i];
+    }
+    return arr;
 }
+
 
 /* Returns array of all the numbers in the column 
  * assumes column is in the range
- * dynamically allocates memory
+ * dynamically allocate memory must be freed
  */
 int* get_column_numbers(Board* b, int col){
-    /* needs to be allocated and freed */
     int *arr = malloc(b->size * sizeof(int));
     int i;
     for(i = 0; i < b->size; i++){
@@ -75,35 +88,97 @@ int* get_column_numbers(Board* b, int col){
     return arr;
 }
 
+/* returns the block index of coordinates */
+int get_coordinate_block_number(Board* b, int x, int y){
+    return y / b->height * b->height + x / b->width;
+}
+
 /* Returns array of all the numbers in the block
  * blocks are defined from up to bottom left to right
  * the number of blocks is width * height
  * assumes block is in the range
- * dynamically allocates memory
+ * dynamically allocate memory must be freed
  */
-int* NOTWORKINGYETget_block_numbers(Board* b, int block_number){
-    /* needs to be allocated and freed */
+int* get_block_numbers(Board* b, int block_number){
     int *arr = malloc(b->size * sizeof(int));
-    int i, j, count = 0;
-    :!a
-
+    int block_y = block_number / b->height;
+    int block_x = block_number - (block_y * b->height);
+    int start_x = b->width * block_x;
+    int start_y = b->height* block_y;
+    int i, j, c = 0;
+    for(i = start_y; i < b->height + start_y; i++){
+        for(j = start_x; j < b->width + start_x; j++){
+            arr[c++] = b->state[i][j];
+        }
+    }
     return arr;
 }
 
+/* checks if number is in the area if true return 1
+ * mode 0 = row
+ * mode 1 = column
+ * mode 2 = block
+ */ 
+int number_in_area(Board* b, int mode, int index, int number){
+    int* arr;
+    int result; 
+    if(mode == 0){
+        arr = get_row_numbers(b, index);
+    }
+    if(mode == 1){
+        arr = get_column_numbers(b, index);
+    }
+    if(mode == 2){
+        arr = get_block_numbers(b, index);
+    }
 
-/* testing thigns */
+    result = number_in_array(arr, number, b->size);
+    free(arr);
+    return result;
+}
+
+/* checks if value can be inserted into x y coordinates return 1 if true */
+int valid_set_value(Board* b, int x, int y, int val){
+    if(!number_in_area(b, 0, y, val) && !number_in_area(b, 1, x, val) && !number_in_area(b, 2, get_coordinate_block_number(b, x, y), val)){
+        return 1;
+            }
+    return 0;
+}
+
+/* sets a cell to value with x, y coordinates
+ * x and y starts at 1
+ */
+void set_cell(Board* b, int x, int y, int val){
+    int i;
+    x = x - 1;
+    y = y - 1;
+    if(b->fixed[x][y] == 1){
+        printf("Error: cell is fixed\n");
+        return;
+    }
+    if(valid_set_value(b, x, y, val)){
+        b->state[x][y] = val;
+    }
+    else{
+        printf("Error: value is invalid\n");
+    }
+}
+
+/* gives hint to cell (x, y) when they start at 1 */
+void hint(Board* b, int x, int y){
+    x = x - 1;
+    y = y - 1;
+    printf("Hint: set cell to %d\n", b->solution[x][y]);
+}
+
+/* testing things */
 int main(){
     Board b2;
     init_board(&b2, 2, 3);
     sample_board(&b2);
-
-    int* arr = get_column_numbers(&b2, 1);
-    for(int i = 0; i < 6; i++){
-        printf("%d ", arr[i]);
-    }
-    printf("\n%d\n", b2.size);
-
-    free(arr);
+    set_cell(&b2, 5, 5 ,2);
+    set_cell(&b2, 5, 6 ,2);
+    hint(&b2, 5, 6);
     free_board(&b2);
     return 0;
 }
@@ -111,6 +186,7 @@ int main(){
 /*
  * create simple board for dubgging
  * initial state should look like:
+ * size 2, 3
  * 1 2 | 3 4 | 5 6
  * 3 4 | 5 6 | 1 2
  * 5 6 | 1 2 | 3 4
