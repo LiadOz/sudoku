380fa5cdb552933daf195eaac8d1ebd705f7abd5
Liad Oz <liadozil@gmail.com>
2020-03-04 18:28:00 +0200

    added num_of_solutions

--- backtrack.c
+++ backtrack.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 #include "stack.h"
 #include "board.h"
+#include "printing.h"
+
 
 /*Return new node of the next empty cell*/
 Node* next_empty_cell(Board* b, int x, int y, int counter) {
-    Node* node = NULL;
-	int stop = 0;
-	int i;
-	int j;
-	for (i = x; i < b->height && !stop; i++) {
-		for (j = y; j < b->width && !stop; j++) {
+	Node* node = NULL;
+	int i, j;
+	for (i = x; i < b->size; i++) {
+		for (j = 0; j < b->size; j++) {
 			if (b->state[i][j] == 0) {
-				stop = 1;
+				node = new_node(i, j, 1, counter);
+				return node;
 			}
 		}
 	}
-	if (i == x && j == y && stop == 0) {
-		return node;
-	}
-	node = new_node(i, j, 1, counter);
 	return node;
 }
 
@@ -28,45 +25,49 @@ Node* first_empty_cell(Board* b) {
 }
 
 int num_of_solutions(Board* b) {
-    Node* temp = NULL;
-	int x;
-	int y;
-	Node** node = NULL;
-	*node = first_empty_cell(b);
+	int x, y;
+	Node* node = first_empty_cell(b);
+	Node* temp = NULL;
+	int counter = 0;
 
 	while (1) {
-		/*stopping condition*/
-		if (*node == NULL) {
-			return pop(node, b);
+		if (!node) {
+			return pop(&node, b);
 		}
 
-		x = (*node)->x;
-		y = (*node)->y;
-
-		while((*node)->value <= b->size) {
-			if (valid_set_value(b, x, y, (*node)->value)) {
-				b->state[x][y] = (*node)->value;
-				temp = next_empty_cell(b, x, y, (*node)->counter);
-				if (temp == NULL) {
-					(*node)->counter++;
-					break;
+		while(node->value <= b->size) {
+			x = node->x; y = node->y;
+			if (valid_set_value(b, x, y, node->value)) {
+				b->state[node->x][node->y] = node->value;
+				temp = next_empty_cell(b, node->x, node->y, node->counter);
+				if (!temp) {
+					node->counter++;
+					counter = pop(&node, b);
+					if (!node) {
+						return counter;
+					}
+					else {
+						node->counter = counter;
+						node->value++;
+					}
 				}
-				temp->prev = *node;
-				*node = temp;
-
+				else {
+					temp->prev = node;
+					node = temp;
+				}
 			}
 			else {
-				(*node)->value++;
+				node->value++;
 			}
 		}
 
-		temp = (*node)->prev;
-		if (temp == NULL) {
-			return pop(node, b);
+		counter = pop(&node, b);
+		if (!node) {
+			return counter;
 		}
 		else {
-			temp->counter = pop(node, b);
-			*node = temp;
+			node->counter = counter;
+			node->value++;
 		}
 	}
 }
