7269c7bd766dc55c4cd31e02609628cd6e022c6b
idanwain <idanwaintraub11@gmail.com>
2020-03-19 20:57:42 +0200

    game over

--- main.c
+++ main.c
@@ -13,16 +13,18 @@ int main(){
     while(1){
         execute_command(&b);
         /* test check if the board is finished */
-		if (b != NULL && (b->filled_cells == (b->size * b->size))) {
-			if (b->wrong_cells == 0) {
-				printf("The puzzle was solved successfully!\n");
-				free(b);
-				b = NULL;
+		if (b->mode != EDIT) {
+			if (b != NULL && (b->filled_cells == (b->size * b->size))) {
+				if (b->wrong_cells == 0) {
+					printf("The puzzle was solved successfully!\n");
+					free(b);
+					b = NULL;
+				}
+				else {
+					printf("Board is errorness, keep trying..\n");
+				}
 			}
-			else {
-				printf("Board is errorness, keep trying..\n");
-			}
-		} 
+		}
     }
     return 1;
 }
